15a22d7f5a273a093ff6affae1d23feeabc03e23
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2023-05-07 20:10:21 +0330

    update

--- Tulip/settings.py
+++ Tulip/settings.py
@@ -119,8 +119,8 @@ if DEBUG:
     STATIC_URL = 'static/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 else:
-    STATIC_URL = 'static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_URL = "/static/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = 'static/media_root/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root")
