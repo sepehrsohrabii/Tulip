8497b0d3c9733ace775de52c60cf27bd4f1817d3
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2023-05-13 23:08:13 +0330

    update

--- Tulip/settings.py
+++ Tulip/settings.py
@@ -103,10 +103,10 @@ if DEBUG:
     STATIC_URL = 'static/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 else:
-    STATIC_URL = 'static/'
+    STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'static/media_root/'
+MEDIA_URL = '/static/media_root/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
